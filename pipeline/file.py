2f1c96634efaffd23b18a8c260c64e9d7b794a0c
Abhimanyu KUMAR <A1QW289Q@india.airtel.itm>
2019-12-14 09:53:49 +0530

    two new files

---
+++ pipeline/file.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver=webdriver.chrome(path="C:\Python37\drivers\chromedriver.exe")
+driver.get("https://www.youtube.com/")
